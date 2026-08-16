aa6d8fa5ada532d97e04714559828144fe463f69
prajenjr <pj1115@srmist.edu>
2026-08-16 19:06:38 +0530

    Connect frontend to live backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://feedly-feedback-rating-portal.onrender.com/api";
 
 const feedbackForm = document.getElementById("feedbackForm");
 const message = document.getElementById("message");
